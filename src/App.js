e05e0a8e77c2193f93a87427527aefd4789e315b
motohirotakagi <tkg.8.mthr@gmail.com>
2021-11-08 18:14:56 +0900

    Titleコンポーネント作成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Title/>
     </div>
   );
 }
---
+++ src/components/TItle.js
@@ -0,0 +1,7 @@
+const Title = () => {
+  return (
+    <h1>コンポーネントテスト</h1>
+  );
+};
+
+export default Title;
